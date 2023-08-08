2a4cc33f174d051b66cdda49a4c4a617f4387796
SruthiGonti <141395823+SruthiGonti@users.noreply.github.com>
2023-08-08 11:31:44 +0530

    Update BrowserSetting.java

--- src/test/java/testcases/mytheresa/BrowserSetting.java
+++ src/test/java/testcases/mytheresa/BrowserSetting.java
@@ -15,8 +15,8 @@ public class BrowserSetting {
 		WebDriver driver = wds.driverSettings();
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(100, TimeUnit.SECONDS);
 		driver.get(BASE_URL);
 		return driver;
 	}
